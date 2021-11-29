97550736b769f0135b3d02cc9e23cbdd00a762bf
paulvxx <89420434+paulvxx@users.noreply.github.com>
2021-11-29 14:34:47 -0700

    Update ProcEntry.c

--- final_project-cs253/src/ProcEntry.c
+++ final_project-cs253/src/ProcEntry.c
@@ -1,1 +1,17 @@
 #include "ProcEntry.h"
+
+void PrintProcEntry(ProcEntry *entry)
+{
+     unsigned long int utime = entry->utime / sysconf(_SC_CLK_TCK);
+     unsigned long int stime = entry->stime / sysconf(_SC_CLK_TCK);
+     fprintf(stdout, "%7d %7d %5c %5lu %5lu %7ld %-25s %-20s\n",
+             entry->pid,
+             entry->ppid,
+             entry->state,
+             utime,
+             stime,
+             entry->num_threads,
+             entry->comm,
+             entry->path);
+}
+
